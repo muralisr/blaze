f41fc69b9282c8c5ddc70300c9f41dc2e102b1a6
Murali Ramanujam <murali.sr92@outlook.com>
2020-03-30 23:09:01 -0700

    Update simulator.py

--- blaze/evaluator/simulator/simulator.py
+++ blaze/evaluator/simulator/simulator.py
@@ -160,7 +160,7 @@ class Simulator:
         for node in completed_this_step:
             self.completed_nodes[node] = self.total_time_ms + node.resource.execution_ms
             # use the following if html should be counted
-            if False and node.resource.url == 'https://www.homedepot.com/' or node.resource.url == 'https://www.nytimes.com/' or node.resource.url == 'https://www.walgreens.com/':
+            if False and (node.resource.url == 'https://www.homedepot.com/' or node.resource.url == 'https://www.nytimes.com/' or node.resource.url == 'https://www.walgreens.com/'):
                 self.log.debug("appending main page to speed_index_time ", viewport=node.resource.viewport_occupied, speed_index_time=self.speed_index_time, appended_time=self.total_time_ms)
                 self.log.debug("landing page time is x and appending with viewport", time=self.total_time_ms,viewport=node.resource.viewport_occupied)
                 self.speed_index_time.append(self.total_time_ms)
